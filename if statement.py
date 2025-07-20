2307b4317661a97db524de1a292701dd983125e9
ashutoshsingh <singhashupratap@gmail.com>
2025-07-20 18:59:38 +0530

    if conditional expression

--- if statement.py
+++ if statement.py
@@ -12,3 +12,22 @@ elif age > 60 :
     print("you are old Sorry ")
 else:
     print("enter correct age between 18 to 60")
+
+
+############## if not
+
+is_sunny = False
+
+if not is_sunny :
+    print("it's hot outside")
+else :
+    print("bhaggggg ")
+
+
+
+a = 10
+b = 20
+print("Positive" if a > b else "Negative")
+
+num = 13
+print("EVEN" if num % 2 == 0 else "ODD")
